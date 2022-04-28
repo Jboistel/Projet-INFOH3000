4656f0ca6efae6b67e95ae5101342518fc7d7615
Jboistel <jboistel@gmail.com>
2022-04-28 22:49:31 +0200

    fonction getData

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
+import csv
+
 global distances
-distances = {}
+distances = []
 
 towns = ['Anderlecht', 'Auderghem', 'Berchem-Sainte-Agathe', 'Bruxelles-Ville', 'Etterbeek', 'Evere', 'Forest',
          'Ganshoren', 'Ixelles', 'Jette', 'Koekelberg', 'Molenbeek-Saint-Jean', 'Saint-Gilles', 'Saint-Josse-ten-Noode',
@@ -7,10 +9,17 @@ towns = ['Anderlecht', 'Auderghem', 'Berchem-Sainte-Agathe', 'Bruxelles-Ville', 
 
 
 def getData(filePath: str):
+    file = open(filePath, mode='r', encoding='utf-8-sig')
+    csvreader = csv.reader(file)
+    count = -1
+    for row in csvreader:
+        distances.append([])
+        count += 1
+        for col in row:
+            distances[count].append(float(col))
     return 0
 
 
 if __name__ == "__main__":
-    list = [5, 9, 3, 4]
-    list.insert(2, 7)
-    print(list)
+    getData("matrix.csv")
+    print(distances)
