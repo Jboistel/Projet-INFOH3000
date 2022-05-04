a06bf602a8cdbe26e0957e000981befc267caf40
jboistel <jboistel@gmail.com>
2022-05-04 13:27:33 +0200

    plot

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
 from Data import Data
+import Solution
+import matplotlib.pyplot as plt
 
-global distances
-distances = []
+solutions: Solution
+
+
+def plot():
+    distances: float
+    risques: float
+    for solution in solutions:
+        distances += solution.calculateDistance()
+        risques += solution.calculateRisque()
+    plt.ylabel("Distance")
+    plt.xlabel("Risque")
+    plt.scatter(distances, risques)
+    plt.show()
 
 
 def main():
     data = Data()
     print(data.getDistMatrix())
+    plot()
 
 
 if __name__ == "__main__":
